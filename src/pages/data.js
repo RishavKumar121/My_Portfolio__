b50fa8f6fa07f6f967d8309aee99c31495b40d23
RishavKumar121 <developerrishav69@gmail.com>
2025-12-15 14:23:11 +0530

    first commit

---
+++ src/pages/data.js
@@ -0,0 +1,131 @@
+import { FaReact } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa6";
+import { FaTools } from "react-icons/fa";
+import { GiSkills } from "react-icons/gi";
+
+
+export const Profile_Data = {
+ name:"Rishav Kumar",
+ tagline : "A full-stack MERN, Web developer passionate about building engaging web applications and solving complex problems with code.",
+ jobTitle :"MERN Stack Developer",
+ location : "India",
+ yearOfExperoence : "6-month of training in MERN stack",
+ email: "rishavkumar137200@gmail.com",
+ phone: "+91 8580624582",
+ website : "Not available yet",
+ GitHub : "https://github.com/RishavKumar121",
+ LinkedIn : "https://www.linkedin.com/in/rishav-kumar-developer",
+ skills:[
+     "React.js",
+        "JavaScript",
+        "HTML5",
+        "CSS3",
+        "Git",
+        "GitHub",
+        "Bootstrap",
+        "TailwindCSS",
+        "Redux",
+        "NodeJs",
+        "Express",
+        "MongoDB"
+ ]
+
+}
+
+export const About_Data = {
+    introduction :"Hi, I'm Rishav, A self-tought MERN Stack Developer B.Tech ( Computer Science And Engineering) student looking for an opportunity in Web Development. Who is committed to study web development. Mindset to fullfill the life with wonderful memories and knowledge.",
+    background:"I hold a Bachelor's degree in Computer Science from Punjab Technical University, Jalandhar.",
+    interests: "Outside of coding, I enjoy traveling to new places, capturing moments through photography, and playing chess. I'm also an avid reader.",
+    careerGoals: "In the future, I aim to continue honing my skills as a developer, exploring new technologies, and contributing to meaningful projects that make a positive impact on people's lives"
+}
+
+export const SKILL = [
+    {
+        title : "Frontend",
+        icon: FaReact ,
+        skills:[
+            {skill:"HTML",percentage:"80%"},
+            {skill:"CSS",percentage:"90%"},
+            {skill:"JavaScript",percentage:"85%"},
+            {skill:"React",percentage:"70%"}
+        ]
+    },
+    {
+        title : "Backend",
+        icon: FaNodeJs ,
+        skills:[
+            {skill:"Node.js",percentage:"70%"},
+            {skill:"Express.js",percentage:"65%"},
+            {skill:"Mongodb",percentage:"70%"}
+        ]
+    },
+    {
+        title : "Tools",
+        icon: FaTools ,
+        skills:[
+            {skill:"Visual Studio Code",percentage:"75%"},
+            {skill:"Git & GitHub",percentage:"83%"},
+            {skill:"Postman",percentage:"50%"}
+        ]
+    },
+    {
+        title : "Soft Skills",
+        icon:  GiSkills ,
+        skills:[
+            {skill:"Problem-Solving",percentage:"80%"},
+            {skill:"Collaboration",percentage:"85%"},
+            {skill:"Attention to Detail",percentage:"75%"}
+        ]
+    },
+]
+
+export const PROJECT = [
+     {
+        title: "E-Commerce Website (MERN Stack)",
+        description: "This full-stack e-commerce platform is built using the MERN stack (MongoDB, Express.js, React.js, Node.js).",
+        GithubLink: "https://github.com/RishavKumar121/_MERN_Project_",
+        LiveServerLink: "https://mern-project-frontend-gyz1.onrender.com/"
+    },
+      {
+        title: "Search countries",
+        description: "RestCountries API fetches country data like population, capital, languages, and currencies. Ideal for developers integrating country-specific info.",
+        GithubLink: "https://github.com/RishavKumar121/Rest-Countries",
+        LiveServerLink: "https://rest-ap.netlify.app/"
+    },
+      {
+        title: "E-commerce cart using react.js",
+        description: "E-commerce app in React using React Router, useContext, and FakeStoreAPI for product listings, detail views, and cart functionality.",
+        GithubLink: "https://github.com/RishavKumar121/Frontent-Ecomm-With-ReduxToolkit",
+        LiveServerLink: "https://project-mern-frontend-pduz.onrender.com/"
+    },
+     {
+        title: "Food Delivery App",
+        description: "Explore our Food Website showcasing menus, services, and event options. Discover our tailored catering solutions designed to elevate every occasion.",
+        GithubLink: "https://github.com/RishavKumar121/Food_Delivery_App",
+        LiveServerLink: "https://food-delivery-app-11.netlify.app/"
+    },
+
+     {
+        title: "World to State Visual Explorer",
+        description: "Explore regions easily by selecting a country and its state to reveal stunning location-specific photos that highlight the beauty and diversity of each area.",
+        GithubLink: "https://github.com/RishavKumar121/Select-Country-State-City",
+        LiveServerLink: "https://select-country-state-city.netlify.app/"
+    },
+
+     {
+        title: "QuickSolve Calculator",
+        description: "A fast, intuitive web calculator created using HTML, CSS, and JavaScript, designed for easy calculations with a sleek layout and user-friendly controls.",
+        GithubLink: "https://github.com/RishavKumar121/Calculator",
+        LiveServerLink: "https://iridescent-lamington-151e9f.netlify.app/"
+    },
+
+     {
+        title: "Translator Using React",
+        description: "A vibrant and responsive React translation tool handling 500 words, packed with intuitive interactions, live feedback, and a stylish, energetic interface.",
+        GithubLink: "https://github.com/RishavKumar121/Translator-with-React",
+        LiveServerLink: "https://translator-12.netlify.app/"
+    },
+
+
+]
+
