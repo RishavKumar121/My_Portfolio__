cfa4da2e58d61248bde5ae3a4472b565c6a7923c
RishavKumar121 <developerrishav69@gmail.com>
2025-12-16 12:18:11 +0530

    Changes in Project and Projectcard and index

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,24 +4,33 @@ import {PROJECT} from "../pages/data"
 import Projectcard from '../pages/Projectcard';
 
 const Project = () => {
-    const settings = {
+  
+  const settings = {
     dots: false,
-    // infinite : true,
-    speed : 500,
-    slidesToShow : 2,
-    slidesToScroll : 1,
-    arrow: false,
-    responsive : [
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    arrows: true,
+
+    responsive: [
       {
-        breakpoint : 769,
-        settings :{
-          slidesToShow: 1,
-          SlidesToscroll:1,
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
         },
       },
-    
-    ]
-  }
+      {
+        breakpoint: 468,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div id='Project' className='max-w-7xl m-auto'>
   <h1 className='text-5xl font-bold text-[#0f9df8] text-center mt-10 py-5'>Projects</h1>
--- src/pages/Projectcard.jsx
+++ src/pages/Projectcard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Projectcard = (prop) => {
+const Projectcard = ({ title, description, GithubLink, LiveServerLink }) => {
   return (
     <div data-aos="fade-down-right" className='bg-[#0e214e] rounded-md border border-[#0a214e] text-center mx-2 h-auto px-5 py-5'>
-      <h1 className='text-2xl font-bold my-2'>{prop.title}</h1>
-      <p>{prop.description}</p>
+      <h1 className='text-2xl font-bold my-2'>{title}</h1>
+      <p>{description}</p>
       <div className='mt-7 '>
-        <a href={prop. GithubLink} target='_blank' className='border border-solid px-5 mx-2 py-3 rounded-md hover:bg-[#1f1f38] cursor-pointer'>Github</a>
-        <a href={prop.LiveServerLink} target='_blank' className='border border-solid px-5 mx-2 py-3 rounded-md hover:bg-[#1f1f38] cursor-pointer'>Live link</a>
+        <a href={GithubLink} target='_blank' className='border border-solid px-5 mx-2 py-3 rounded-md hover:bg-[#1f1f38] cursor-pointer'>Github</a>
+        <a href={LiveServerLink} target='_blank' className='border border-solid px-5 mx-2 py-3 rounded-md hover:bg-[#1f1f38] cursor-pointer'>Live link</a>
       </div>
     </div>
   )
